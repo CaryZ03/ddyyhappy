49666e0ced8da45257fdc33bb0492c2354c2f89e
maybeAlways <112628774+maybeAlways@users.noreply.github.com>
2023-09-01 01:28:28 +0800

    css

--- ddyytest/src/components/Home.vue
+++ ddyytest/src/components/Home.vue
@@ -1,45 +1,83 @@
 <template>
-  <div class="header-main" :style="getColor()">
-    <div class="h5-header">
-        <div class="header-left"><i class="okicon icon-0121"></i></div>
-        <div class="header-right">
-            <span :class="{active: active == 0}">Home</span>
-            <span :class="{active: active == 1}">Guidance</span>
-            <span :class="{active: active == 1}">WorkSpace</span>
-            <span :class="{active: active == 2}">Login</span>
-        </div>
+  <div>
+    <div class="nav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Guidance</a></li>
+        <li><a href="#">WorkSpace</a></li>
+        <li><a href="#">Login</a></li>
+        <div class="nav-box"></div>
+      </ul>
     </div>
   </div>
 </template>
 
 <style>
-.header-main{
-  min-width: 1200px;
-  background:rgba(255, 255, 255, 0.15);
-  position: fixed;
-  top:0;
-  left: 0;
-  right: 0;
+*{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    text-decoration: none;
+}
+body{
+    height: 3000px;
+    background-color: #8da1f8;
 }
 
-.active{
-  color: #FFA624;
-  border-bottom: 4px solid #FFA624;
-  font-weight: 800;
+.nav{
+    width: 100%;
+    height: 4.5vw;
+    position: fixed;
+    top: 0;
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.5);
+    border-radius: 0 0 10px 10px;
+    background-color: #fff;
+}
+.nav ul{
+    width: 40%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    right: 5%;
+    margin-left: 60%;
+}
+.nav ul .nav-box{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    /* 如果导航栏就六个导航，那么每个导航的宽度都是整个导航
+    的六分之一 */
+    width: calc((40% / 4)*1);
+    height: 0.8vw;
+    border-radius: 10px;
+    transition: .5s;
+}
+.nav ul li{
+    width: 100%;
+    text-align: center;
+}
+.nav ul li a{
+    color: rgb(70, 100, 180);
+    font: 1.6vw '优设标题黑';
+    display: block;
+    width: 100%;
+    height: 100%;
+    line-height: 70px;
+}
+.nav ul li:nth-child(1):hover~.nav-box{
+    left: calc(40% / 4 *0 + 60%);
+    background-color: rgb(250, 190, 250);
+}
+.nav ul li:nth-child(2):hover~.nav-box{
+    left: calc(40% / 4 *1 + 60%);
+    background-color: red;
+}
+.nav ul li:nth-child(3):hover~.nav-box{
+    left: calc(40% / 4 *2 + 60%);
+    background-color: #d18df8;
+}
+.nav ul li:nth-child(4):hover~.nav-box{
+    left: calc(40% / 4 *3 + 60%);
+    background-color: #ffb1b1;
 }
 </style>
-
-<script>
-export default {
-  getColor() {
-    //当滚动距离超过导航栏高度时改变背景色
-    if (this.scrollTop > 45) {
-      return {
-        background: "#487DFE"
-      }
-    } else {
-      return false
-    }
-  }
-}
-</script>
--- ddyytest/src/views/HomeView.vue
+++ ddyytest/src/views/HomeView.vue
@@ -1,59 +1,132 @@
 <template>
-    <div class="hello">
-      <h1>{{ msg }}</h1>
-      <p>
-        For a guide and recipes on how to configure / customize this project,<br>
-        check out the
-        <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-      </p>
-      <h3>Installed CLI Plugins</h3>
+  <div>
+    <div class="nav">
+    <div class="logo">Unknown</div> 
       <ul>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      </ul>
-      <h3>Essential Links</h3>
-      <ul>
-        <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-        <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-        <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-        <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-      </ul>
-      <h3>Ecosystem</h3>
-      <ul>
-        <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-        <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-        <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-        <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Guidance</a></li>
+        <li><a href="#">WorkSpace</a></li>
+        <li><a href="#">Login</a></li>
+        <div class="nav-box"></div>
       </ul>
     </div>
-  </template>
-  
-  <script>
+    <div class="float-start-button">
+      <span>即刻开始</span>
+    </div>
+  </div>
+</template>
+
+<script>
   export default {
     name: 'HomeView',
     props: {
       msg: String
     }
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
+</script>
+
+<style>
+*{
+    list-style: none;
     padding: 0;
+    margin: 0;
+    text-decoration: none;
+}
+body{
+    height: 3000px;
+    background-color: #8da1f8;
+}
+
+.nav{
+    width: 100%;
+    height: 4.5vw;
+    position: fixed;
+    display: flex;
+    top: 0;
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.5);
+    border-radius: 0 0 10px 10px;
+    background-color: #fff;
+}
+
+.logo{
+  width: 10%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  right: 5%;
+  margin-left: 3%;
+  color: rgb(70, 100, 180);
+}
+
+.nav ul{
+    width: 40%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    right: 5%;
+    margin-left: 47%;
+}
+.nav ul .nav-box{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    /* 如果导航栏就六个导航，那么每个导航的宽度都是整个导航
+    的六分之一 */
+    width: calc((40% / 4)*1);
+    height: 0.8vw;
+    border-radius: 10px;
+    transition: .5s;
+}
+.nav ul li{
+    width: 100%;
+    text-align: center;
+}
+.nav ul li a{
+    color: rgb(70, 100, 180);
+    font: 1.6vw '优设标题黑';
+    display: block;
+    width: 100%;
+    height: 100%;
+    line-height: 70px;
+}
+.nav ul li:nth-child(1):hover~.nav-box{
+    left: calc(40% / 4 *0 + 60%);
+    background-color: rgb(250, 190, 250);
+}
+.nav ul li:nth-child(2):hover~.nav-box{
+    left: calc(40% / 4 *1 + 60%);
+    background-color: red;
+}
+.nav ul li:nth-child(3):hover~.nav-box{
+    left: calc(40% / 4 *2 + 60%);
+    background-color: #d18df8;
+}
+.nav ul li:nth-child(4):hover~.nav-box{
+    left: calc(40% / 4 *3 + 60%);
+    background-color: #ffb1b1;
+}
+.float-start-button {
+  position: fixed;
+  right: 0;
+  top: 70%;
+  z-index: 6666;
+  padding: 1.5vw;
+  width: 10vw;
+  opacity: 1;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 8px 0px 0px 8px;
+  box-shadow: 0px 2px 15px 0px rgba(9,41,77,0.15);
+  cursor: move;
+  span {
+    font-size: 800px;
+    color: #333333;
+    text-align: center;
   }
-  li {
-    display: inline-block;
-    margin: 0 10px;
+}
+.float-start-button span {
+    font-size: 1.25vw;
+    color: #333333;
+    text-align: center;
   }
-  a {
-    color: #42b983;
-  }
-  </style>
+</style>
   
